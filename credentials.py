fcf84e1defb95b075d2d38f2704f745f49a89f16
kingsley bingala <kingsleybingala@gmail.com>
2020-08-10 00:12:40 +0200

    Enviroment Hidden, ignore fixed

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'n9ChmY1FVeo0b4cWaX1bkJNzp'
+consumer_secret = 'docgRZkMLaYFgHhZa7KP34WXdMgIWZXYu1JPddjUZThkBNMHeU'
+access_token = '1115193054615437313-rvdMolYFi6G8hfwLDgBJEf0QMI4KrI'
+access_token_secret = 'OJHta58AfqF4QTZxSG7twHriKl2uUDVnPOqwVsBndYEiP'
